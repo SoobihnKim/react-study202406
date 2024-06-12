68d3e177c6bf48e13961f15a48c5ca91dccd2ed4
soo <ksbsj92@naver.com>
2024-06-12 10:47:15 +0900

    # 동적 데이터 렌더링

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import Hello from "./components/Hello";
-import Bye from "./components/Bye";
-import Greet from "./components/Greet";
 
-function App() {
+import './components/expenses/ExpenseItem.css'
+import ExpenseItem from "./components/expenses/ExpenseItem";
+
+const App = () => {
     // jsx 문법
     // const $h2 = React.createElement('h2', null, '방가방가햄토리');
-    const $h2 = <h2>방가방가햄토리</h2>;
+    // const $h2 = <h2>방가방가햄토리</h2>;
 
     // jsx 규칙
     // 1. return 안에 있는 태그는 반드시 하나의 태그로 묶여야 함.
@@ -17,22 +17,13 @@ function App() {
     // 4. 의미없는 부모는 <React.Fragment>로 감싸면 됨. <>로도 가능함.
     // 5. 변수값이나 함수를 출력할 때는 {}로 감싸면 됨.
 
-    const hello = '안녕안녕?';
+    // const hello = '안녕안녕?';
 
     return (
         <>
-            <Bye />
-            <Hello />
-            <Bye />
-            <Greet />
-            {/*<div className="App">*/}
-            {/*    <h1>{hello}</h1>*/}
-            {/*    {$h2}*/}
-            {/*</div>*/}
-            {/*<div className='noname'>*/}
-            {/*    <input type="text"/>*/}
-            {/*    <label htmlFor=""></label>*/}
-            {/*</div>*/}
+            <ExpenseItem />
+            <ExpenseItem />
+
         </>
     );
 }
