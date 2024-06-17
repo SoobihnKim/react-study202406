3705f37b1b886661b3436dd76892fae901f66d3c
SoobihnKim <ksbsj92@naver.com>
2024-06-17 12:25:55 +0900

    # useRef Hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, useRef, useState} from 'react';
 import './App.css';
 import AddUsers from './components/Users/AddUsers';
 import UserList from "./components/Users/UserList";
 import ErrorModal from "./components/UI/Modal/ErrorModal";
+import addUsers from "./components/Users/AddUsers";
 
 const App = () => {
 
     // 회원들이 저장될 배열
     const [userList, setUserList] = useState([]);
 
+    // 기억을 해놨다가 증가시킬 수 있음(useState는 바로바로 반영되지않음)
+    const count = useRef(1);
+    console.log('count: ', count);
+
     const addUserHandler = user => {
+
+        count.current++;
+        console.log('count.current: ', count.current);
+
         console.log(user); // 신규 user
         setUserList(prev => [
             ...prev,
