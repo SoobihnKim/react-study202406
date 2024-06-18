a11695e1a4da7478eb63b5d6ff690c28d84c4692
SoobihnKim <ksbsj92@naver.com>
2024-06-18 11:59:00 +0900

    # useReducer로 상태관리 중앙집중화하기 (주석 정리)

--- src/components/SideEffect/Login.js
+++ src/components/SideEffect/Login.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useReducer, useState} from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 
 import Card from '../UI/Card';
 import styles from './Login.module.css';
@@ -11,93 +11,77 @@ import Button from '../UI/Button';
   컴포넌트 바깥쪽에 선언하는게 일반적입니다.
 
   param1 - state : 변경 전의 상태값
-  param2 - action : dispatch 함수(상태변경 등의 행동)가 전달한 상태변경객체,
+  param2 - action : dispatch함수(상태변경 등의 행동)가
+                    전달한 상태변경객체,
                     지금 어떤 상태값을 변경하는가? - type
-                    어떤 값으로 변경하는지? - value
+                    어떤 값으로 변경하는지?        - value
   return : 변경 후의 상태값
- */
+*/
 
 const emailReducer = (state, action) => {
-    // console.log('email reducer call!');
-    // console.log('state: ', state); // 변경 전 상태 객체
-    // console.log('action: ', action); // 지금 상태 변경이 일어난 객체
+    // console.log('email reducer call!!');
+    // console.log('state: ', state);  // 변경 전 상태객체
+    // console.log('action: ', action); // 지금 상태변경이 일어난 객체
 
-    // return { inputValue: action.val }; // 변경 후 상태 객체
     if (action.type === 'USER_INPUT') {
         return {
             inputValue: action.val,
-            isValid: action.val.includes('@')
+            isVaild: action.val.includes('@')
         };
     } else if (action.type === 'VALIDATE') {
         return {
             inputValue: state.inputValue,
-            isValid: state.inputValue.includes('@')
+            isVaild: state.inputValue.includes('@')
         };
     }
 };
 
 const passwordReducer = (state, action) => {
-    // console.log('email reducer call!');
-    // console.log('state: ', state); // 변경 전 상태 객체
-    // console.log('action: ', action); // 지금 상태 변경이 일어난 객체
 
-    // return { inputValue: action.val }; // 변경 후 상태 객체
-    if (action.type === 'USER_INPUT') { // 체인지 이벤트
+    if (action.type === 'USER_INPUT') {
         return {
             inputValue: action.val,
-            isValid: action.val.trim().length > 6
+            isVaild: action.val.trim().length > 6
         };
-    } else if (action.type === 'VALIDATE') { // 블러 이벤트
+    } else if (action.type === 'VALIDATE') {
         return {
             inputValue: state.inputValue,
-            isValid: state.inputValue.trim().length > 6
+            isVaild: state.inputValue.trim().length > 6
         };
     }
 };
 
-
-const Login = ({onLogin}) => {
-
-    // console.log('렌더링 수행'); // 한 글자 입력할 때마다 수행됨
+const Login = ({ onLogin }) => {
+    // console.log('렌더링 수행!');
 
     // email reducer로 이메일 상태관리하기
     /*
-     param1: 위에서 만든 리듀서 함수
-     param2: 상태값의 초기값
-     return: 리듀서를 관리하는 배열
-        [0]: 이메일 관련 상태값
-        [1]: 상태를 변경할 수 있는 함수
-     */
+      param1: 위에서 만든 리듀서 함수
+      param2: 상태값의 초기값
+      return: 리듀서를 관리하는 배열
+          [0]: 이메일 관련 상태값
+          [1]: 상태를 변경할 수 있는 함수
+    */
     const [emailState, dispatchEmail] = useReducer(emailReducer, {
         inputValue: '',
-        isValid: null
+        isValid: null,
     });
     // console.log('abc: ', abc);
-    // console.log('변경 후 이메일 상태: ', emailState);
+    // console.log('변경 후 이메일상태: ', emailState);
 
     const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
         inputValue: '',
-        isValid: null
+        isValid: null,
     });
 
-    // 사용자가 입력한 이메일을 상태 관리
-    // const [enteredEmail, setEnteredEmail] = useState('');
-    // 이메일 입력 값이 정상인지 확인
-    // const [emailIsValid, setEmailIsValid] = useState();
-
-
-    // 사용자가 입력한 패스워드를 상태 관리
-    const [enteredPassword, setEnteredPassword] = useState('');
-    // 패스워드 입력 값이 정상인지 확인
-    const [passwordIsValid, setPasswordIsValid] = useState();
     // 이메일, 패스워드가 둘 다 정상인지 확인
     const [formIsValid, setFormIsValid] = useState(false);
 
     const emailChangeHandler = (e) => {
         // setEnteredEmail(e.target.value);
 
-        // reducer의 상태 변경은 반드시 dispatch 함수를 통해 처리
-        // dispatch 호출 시 리듀서 함수가 호출됨
+        // reducer의 상태 변경은 반드시 dispatch함수를 통해 처리
+        // dispatch 호출시 리듀서함수가 호출됨.
 
         // param1: 리듀서 함수의 action에 전달할 내용
         dispatchEmail({
@@ -108,59 +92,60 @@ const Login = ({onLogin}) => {
     };
 
     const passwordChangeHandler = (e) => {
-        setEnteredPassword(e.target.value);
+        dispatchPassword({
+            type: 'USER_INPUT',
+            val: e.target.value
+        });
 
-        // setFormIsValid(
-        //     e.target.value.trim().length > 6 && enteredEmail.includes('@')
-        // );
     };
 
     const validateEmailHandler = () => {
         // setEmailIsValid(enteredEmail.includes('@'));
-
         dispatchEmail({
             type: 'VALIDATE'
         });
     };
 
-
     const validatePasswordHandler = () => {
-        setPasswordIsValid(enteredPassword.trim().length > 6);
+        dispatchPassword({
+            type: 'VALIDATE'
+        });
     };
 
     // 로그인 버튼을 눌렀을 때 이벤트 핸들러
     const submitHandler = (e) => {
         e.preventDefault();
-        // App.js에서 받은 로그인 핸들러 호출
+        // App.js에서 받은 로그인핸들러 호출
         onLogin(emailState.inputValue, passwordState.inputValue);
     };
 
-    // 비동기
-    useEffect(() => { // 보통 fetch 들어감
+    // emailState와 passwordState에서 isValid 추출
+    const { isVaild: emailIsValid } = emailState;
+    const { isVaild: passwordIsValid } = passwordState;
 
-        // 디바운싱 Debouncing 마지막 입력 후 한 번 실행
+    useEffect(() => {
         const timer = setTimeout(() => {
             console.log('useEffect call in Login.js');
             setFormIsValid(
-                passwordState.isValid && emailState.isValid
+                emailIsValid && passwordIsValid
             );
         }, 1000);
 
-        // clean up 함수는 컴포넌트가 업데이트되거나 사라지기 전에 실행
+        // clean up함수는 컴포넌트가 업데이트되거나 사라지기 전에 실행
         return () => {
-            // console.log('clean up: ', enteredEmail); // 변경 전 값을 가지고있음
+            // console.log('cleanup: ', enteredEmail);
             clearTimeout(timer);
         };
-    }, []); // 의존성 배열
+    }, [emailIsValid, passwordIsValid]);
 
-    // console.log('render: ', enteredEmail); // 변경된 값을 가지고있음
+    // console.log('render: ', enteredEmail);
 
     return (
         <Card className={styles.login}>
             <form onSubmit={submitHandler}>
                 <div
                     className={`${styles.control} ${
-                        emailState.isValid === false ? styles.invalid : ''
+                        !emailIsValid ? styles.invalid : ''
                     }`}
                 >
                     <label htmlFor="email">E-Mail</label>
@@ -169,12 +154,12 @@ const Login = ({onLogin}) => {
                         id="email"
                         value={emailState.inputValue}
                         onChange={emailChangeHandler}
-                        onBlur={validateEmailHandler} // 포커싱을 잃었을때
+                        onBlur={validateEmailHandler}
                     />
                 </div>
                 <div
                     className={`${styles.control} ${
-                        passwordState.isVaild === false ? styles.invalid : ''
+                        !passwordIsValid ? styles.invalid : ''
                     }`}
                 >
                     <label htmlFor="password">Password</label>
@@ -187,10 +172,11 @@ const Login = ({onLogin}) => {
                     />
                 </div>
                 <div className={styles.actions}>
-                    <Button type="submit"
-                            className={styles.btn}
-                        // 둘다 조건 맞으면 버튼 활성화됨
-                            disabled={!formIsValid}>
+                    <Button
+                        type="submit"
+                        className={styles.btn}
+                        disabled={!formIsValid}
+                    >
                         Login
                     </Button>
                 </div>
