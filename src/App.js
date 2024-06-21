8edf49cb0554ff9ba4774279a2862a590b06e4b0
SoobihnKim <ksbsj92@naver.com>
2024-06-21 10:49:59 +0900

    # Cart App - Cart Context 정의

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Header from './components/Food/Layout/Header';
 import Meals from './components/Food/Meals/Meals';
+import Cart from "./components/Food/Cart/Cart";
+import CartContext from "./store/cart-context";
+import CartProvider from "./store/CartProvider";
 
 const App = () => {
 
     return (
-        <>
+        <CartProvider>
+            <Cart />
             <Header />
             <div id="main">
                 <Meals />
             </div>
-        </>
+        </CartProvider>
     );
 };
 
