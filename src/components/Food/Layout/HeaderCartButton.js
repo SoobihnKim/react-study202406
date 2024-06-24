944cb6d920894324aed190d0ede7c5e207ae0ea9
SoobihnKim <ksbsj92@naver.com>
2024-06-24 10:24:27 +0900

    # Cart App - 장바구니 아이템 헤더에 총 수량 배지 렌더링

--- src/components/Food/Layout/HeaderCartButton.js
+++ src/components/Food/Layout/HeaderCartButton.js
@@ -8,13 +8,23 @@ const HeaderCartButton = ({ onShow }) => {
     // const { totalAmount } = useContext(CartContext);
     const { cartItems } = useContext(CartContext);
 
-    const calcTotalAmount = () => {
-        let totalAmount = 0;
-        for (const item of cartItems) {
-            totalAmount += item.amount;
-        }
-        return totalAmount;
-    };
+    const numberOfCart = cartItems.reduce((accum, current) => {
+        // console.log('accum: ', accum);
+        // console.log('current: ', current);
+        return accum + current.amount;
+    }, 0);
+
+    // 한줄로 정리
+    // const numberOfCart = cartItems.reduce((accum, current) => accum + current.amount, 0);
+
+
+    // const calcTotalAmount = () => {
+    //     let totalAmount = 0;
+    //     for (const item of cartItems) {
+    //         totalAmount += item.amount;
+    //     }
+    //     return totalAmount;
+    // };
 
     const { button, icon, badge } = styles;
 
@@ -25,7 +35,7 @@ const HeaderCartButton = ({ onShow }) => {
       </span>
             <span>My Cart</span>
             {/*<span className={badge}>{totalAmount}</span>*/}
-            <span className={badge}>{calcTotalAmount()}</span>
+            <span className={badge}>{numberOfCart}</span>
         </button>
     );
 };
