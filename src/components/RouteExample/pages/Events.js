38a50b2f1d18167faf074061a43e0b8dc32238d0
SoobihnKim <ksbsj92@naver.com>
2024-07-03 12:03:30 +0900

    # 에러 페이지

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -9,6 +9,8 @@ const Events = () => {
     const eventList = useLoaderData();
     console.log(eventList);
 
+    console.log('event page rendering!');
+
     return (
         <>
             <h1>Events Page</h1>
@@ -23,14 +25,29 @@ export default Events;
 // loader를 app.js로부터 아웃소싱
 export const loader = async () => {
 
+    console.log('loader call'); // loader는 항상 먼저 작동됨
+
     // 이 페이지가 열릴 때 자동으로 트리거되어 호출되는 함수
     // 이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있습니다.
 
-    const response = await fetch('http://localhost:8282/events');
+    const response = await fetch('http://localhost:8282/events?sort=date');
     // const jsonData = await response.json();
+    // console.log(response.status); // 상태코드 출력
 
-    // loader가 리턴한 데이터는 loader를 선언한 컴포넌트와 그 하위 컴포넌트에서 언제든 불러서 사용 가능
+    // 예외 처리
+    if (!response.ok) {
+        // 백에서 에러메시지 가져옴
+        const errorText = await response.text();
 
-   // loader에서 fetch의 결과를 바로 리턴하면 알아서 json을 추출해줌(loader에서만 가능함)
-    return response;
+        throw new Response(
+            JSON.stringify({message: errorText}),
+            {
+                status: response.status
+            }
+        ); // return 말고, 코드 흐름을 끊어줘야함
+        // return response; // error message
+    }
+
+    // loader에서 fetch의 결과를 바로 리턴하면 알아서 json을 추출해줌(loader에서만 가능함)
+    return response; // ok일 경우 events[]
 };
