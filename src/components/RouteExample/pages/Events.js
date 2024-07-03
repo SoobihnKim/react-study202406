83f9d37a79c324a0ae55010c930c14696f7455ea
SoobihnKim <ksbsj92@naver.com>
2024-07-03 10:46:14 +0900

    # loader 기초 사용법

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,56 +1,33 @@
 import React, {useEffect, useState} from 'react';
-import {Link} from "react-router-dom";
+import {Link, useLoaderData} from "react-router-dom";
 import EventList from "../components/EventList";
 import EventNavigation from "../layout/EventNavigation";
 
 // DUMMY_EVENTS 포스트맨에 입력해서 데이터베이스에 조회되면 지우기
-// const DUMMY_EVENTS = [
-//     {
-//         id: '1',
-//         title: '여름맞이 이벤트',
-//         image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDXvjjsw8G84qqSWC2Kdnn5r1cmXlZ3Js7Ng&s',
-//         date: '2024-07-20',
-//         description: '여름맞이 이벤트입니다.'
-//     },
-// ];
 
 const Events = () => {
 
-    const [eventList, setEventList] = useState([]);
+    // loader가 리턴한 데이터 받아오기
+    const eventList = useLoaderData();
+    console.log(eventList);
 
-    useEffect(() => {
-        (async () => {
-            const response = await fetch('http://localhost:8282/events');
-            const jsonData = await response.json();
-            setEventList(jsonData);
-        })();
+    // App.js loader로 이동하면서 상태변수 못씀
+    // const [eventList, setEventList] = useState([]);
 
-    }, []);
-
-    // 렌더링 하기 전 최초 한번만 실행하면 됨
+    // App.js loader로 이동
     // useEffect(() => {
-    //     const fetchEvents = async () =>{
-    //         const response = await fetch();
+    //     (async () => {
+    //         const response = await fetch('http://localhost:8282/events');
     //         const jsonData = await response.json();
     //         setEventList(jsonData);
-    //     };
-    //
-    //     fetchEvents();
-    //
-    //     fetch('http://localhost:8282/events')
-    //         .then(res => res.json())
-    //         .then(jsonData => {
-    //             console.log(jsonData);
-    //             setEventList(jsonData); // (useEffect 사용 안할 때)상태변수의 변경 -> 리렌더링 -> 무한루프
-    //         });
-    // }, []); // 의존성 배열 비워두면 한번만 실행됨
+    //     })();
+    // }, []);
 
     return (
         <>
             <h1>Events Page</h1>
             {/*<EventNavigation />*/}
             <EventList eventList={eventList}/>
-
         </>
     );
 };
