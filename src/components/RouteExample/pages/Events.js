fabc8d188b71f96e1ca64359fab6ce6229b37d22
SoobihnKim <ksbsj92@naver.com>
2024-07-03 10:03:55 +0900

    # 서버에서 데이터 GET으로 가져오기

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -1,36 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 
-const DUMMY_EVENTS = [
-    {
-        id: '1',
-        title: '여름맞이 이벤트',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDXvjjsw8G84qqSWC2Kdnn5r1cmXlZ3Js7Ng&s',
-        date: '2024-07-20',
-        description: '여름맞이 이벤트입니다.'
-    },
-    {
-        id: '2',
-        title: '여름맞이 할인 이벤트',
-        image: 'https://m.puroluna.com/file_data/iffl8888/2019/06/18/e6a78ac2fbfa26acf240feb86380a47f.jpg',
-        date: '2024-08-20',
-        description: '여름맞이 할인 이벤트입니다.'
-    },
-];
+// DUMMY_EVENTS 포스트맨에 입력해서 데이터베이스에 조회되면 지우기
+// const DUMMY_EVENTS = [
+//     {
+//         id: '1',
+//         title: '여름맞이 이벤트',
+//         image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDXvjjsw8G84qqSWC2Kdnn5r1cmXlZ3Js7Ng&s',
+//         date: '2024-07-20',
+//         description: '여름맞이 이벤트입니다.'
+//     },
+// ];
 
 const Events = () => {
+
+    const [eventList, setEventList] = useState([]);
+
+        useEffect(() => {
+            (async () => {
+                const response = await fetch('http://localhost:8282/events');
+                const jsonData = await response.json();
+                setEventList(jsonData);
+            })();
+
+        }, []);
+
+    // 렌더링 하기 전 최초 한번만 실행하면 됨
+    // useEffect(() => {
+    //     const fetchEvents = async () =>{
+    //         const response = await fetch();
+    //         const jsonData = await response.json();
+    //         setEventList(jsonData);
+    //     };
+    //
+    //     fetchEvents();
+    //
+    //     fetch('http://localhost:8282/events')
+    //         .then(res => res.json())
+    //         .then(jsonData => {
+    //             console.log(jsonData);
+    //             setEventList(jsonData); // (useEffect 사용 안할 때)상태변수의 변경 -> 리렌더링 -> 무한루프
+    //         });
+    // }, []); // 의존성 배열 비워두면 한번만 실행됨
+
     return (
         <>
-        <h1>Events Page</h1>
-        <ul>
-            {
-                DUMMY_EVENTS.map(event => (
+            <h1>Events Page</h1>
+            <ul>
+                {eventList.map((event) => (
                     <li key={event.id}>
                         <Link to={event.id}>{event.title}</Link>
                     </li>
-                ))
-            }
-        </ul>
+                ))}
+            </ul>
+
         </>
     );
 };
