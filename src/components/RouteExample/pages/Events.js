d7409123458e52ea8c81d7600c19c78f640dd1c0
SoobihnKim <ksbsj92@naver.com>
2024-07-03 11:35:06 +0900

    # loader를 app.js로부터 아웃소싱

--- src/components/RouteExample/pages/Events.js
+++ src/components/RouteExample/pages/Events.js
@@ -3,25 +3,11 @@ import {Link, useLoaderData} from "react-router-dom";
 import EventList from "../components/EventList";
 import EventNavigation from "../layout/EventNavigation";
 
-// DUMMY_EVENTS 포스트맨에 입력해서 데이터베이스에 조회되면 지우기
-
 const Events = () => {
 
     // loader가 리턴한 데이터 받아오기
     const eventList = useLoaderData();
     console.log(eventList);
-
-    // App.js loader로 이동하면서 상태변수 못씀
-    // const [eventList, setEventList] = useState([]);
-
-    // App.js loader로 이동
-    // useEffect(() => {
-    //     (async () => {
-    //         const response = await fetch('http://localhost:8282/events');
-    //         const jsonData = await response.json();
-    //         setEventList(jsonData);
-    //     })();
-    // }, []);
 
     return (
         <>
@@ -33,3 +19,18 @@ const Events = () => {
 };
 
 export default Events;
+
+// loader를 app.js로부터 아웃소싱
+export const loader = async () => {
+
+    // 이 페이지가 열릴 때 자동으로 트리거되어 호출되는 함수
+    // 이 함수에는 페이지가 열리자마자 해야할 일을 적을 수 있습니다.
+
+    const response = await fetch('http://localhost:8282/events');
+    // const jsonData = await response.json();
+
+    // loader가 리턴한 데이터는 loader를 선언한 컴포넌트와 그 하위 컴포넌트에서 언제든 불러서 사용 가능
+
+   // loader에서 fetch의 결과를 바로 리턴하면 알아서 json을 추출해줌(loader에서만 가능함)
+    return response;
+};
