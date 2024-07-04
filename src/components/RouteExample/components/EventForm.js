85655e3188240c075617a6d165867592e9621b6c
SoobihnKim <ksbsj92@naver.com>
2024-07-04 11:47:11 +0900

    # 백엔드 연동 - 서버에 POST요청 보내기

--- src/components/RouteExample/components/EventForm.js
+++ src/components/RouteExample/components/EventForm.js
@@ -5,12 +5,37 @@ import {useParams, useNavigate} from "react-router-dom";
 
 const EventForm = ({ method, event={} }) => {
 
+    // console.log(event);
+
     const {
         title,
         desc: description,
         'img-url': image,
         'start-date': date
     } = event;
+
+    // 날짜 형식을 변경 (yyyy-MM-dd)
+    /**
+     *
+     * @param date - yyyy년 MM월 dd일
+     */
+    const convertDateFormat = (date) => {
+        const [yearPart, monthDayPart] = date.split('년 ');
+        const [monthPart, dayPart] = monthDayPart.split('월 ');
+
+        const day = dayPart.replace('일', '');
+
+        // console.log('date: ', { yearPart, monthPart, day });
+
+        return `${yearPart}-${monthPart}-${day}`;
+    };
+
+    let formatDate;
+    if (event.date) {
+        formatDate = convertDateFormat(date);
+    }
+
+
 
     // const { eventId : id} = useParams();
     const navigate = useNavigate();
@@ -21,9 +46,42 @@ const EventForm = ({ method, event={} }) => {
         navigate('..');
     };
 
+    const submitHandler = e => {
+        e.preventDefault(); // 새로고침 방지
+        // console.log('form 제출');
+
+        // input에 입력한 값 가져오기
+        const formData = new FormData(e.target);
+        // console.log('form: ', formData.get('title'));
+
+        // 서버에 보낼 데이터
+        const payload = {
+            title: formData.get('title'),
+            desc: formData.get('description'),
+            imageUrl: formData.get('image'),
+            beginDate: formData.get('date')
+        };
+
+        // console.log('payload: ', payload);
+
+        // 서버로 페칭
+        (async () => {
+            const response = await fetch(`http://localhost:8282/events`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            });
+
+            navigate('/events');
+        }) ();
+
+    };
+
 
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={submitHandler} noValidate>
             <p>
                 <label htmlFor="title">Title</label>
                 <input id="title" type="text" name="title" required defaultValue={event ? title : ''} />
@@ -34,7 +92,7 @@ const EventForm = ({ method, event={} }) => {
             </p>
             <p>
                 <label htmlFor="date">Date</label>
-                <input id="date" type="date" name="date" required defaultValue={event ? date : ''}/>
+                <input id="date" type="date" name="date" required  defaultValue={event ? formatDate : ''}/>
             </p>
             <p>
                 <label htmlFor="description">Description</label>
