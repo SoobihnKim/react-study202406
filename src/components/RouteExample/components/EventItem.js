537af97ce59904fd94affdb90681a006022bc120
SoobihnKim <ksbsj92@naver.com>
2024-07-05 10:00:01 +0900

    # 백엔드 연동 - 이벤트 삭제 클릭 이벤트

--- src/components/RouteExample/components/EventItem.js
+++ src/components/RouteExample/components/EventItem.js
@@ -1,28 +1,58 @@
 import React from "react";
 import styles from './EventItem.module.scss';
-import {Link} from "react-router-dom";
+import {Link, redirect, useNavigate, useParams} from "react-router-dom";
 
-const EventItem = ({ event }) => {
+const EventItem = ({event}) => {
 
     const {
+        'event-id': id,
         title,
         desc: description,
         'img-url': image,
         'start-date': date
     } = event;
 
+    // const { eventId: id } = useParams();
+    const navigate = useNavigate();
+
+    // const fetchDelete = async () => {
+    //     const res = await fetch(`http://localhost:8282/events/${id}`, {
+    //         method: 'DELETE'
+    //     });
+    // };
+
+    const deleteHandler = e => {
+
+        if (!window.confirm('정말 삭제하시겠습니까?')) return;
+
+        console.log('id: ', id);
+
+        (async () => {
+            await fetch(`http://localhost:8282/events/${id}`, {
+                method: 'DELETE'
+            });
+        })();
+
+        // fetchDelete();
+
+        setTimeout(() => {
+            navigate('/events');
+        }, 200);
+    };
+
     return (
         <article className={styles.event}>
-            <img src={image} alt={title} />
+            <img src={image} alt={title}/>
             <h1>{title}</h1>
             <time>{date}</time>
             <p>{description}</p>
             <menu className={styles.actions}>
-                <Link  to="edit">Edit</Link>
-                <button>Delete</button>
+                <Link to="edit">Edit</Link>
+                <button onClick={deleteHandler}>Delete</button>
             </menu>
         </article>
     );
 };
+
 
 export default EventItem;
