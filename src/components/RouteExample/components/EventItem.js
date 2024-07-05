104016cff55321a1d2b324f5a06f1ad86e6d3b5e
SoobihnKim <ksbsj92@naver.com>
2024-07-05 10:22:17 +0900

    # 백엔드 연동 - action 함수로 삭제하기(useSubmit)

--- src/components/RouteExample/components/EventItem.js
+++ src/components/RouteExample/components/EventItem.js
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from './EventItem.module.scss';
-import {Link, redirect, useNavigate, useParams} from "react-router-dom";
+import {Link, redirect, useNavigate, useParams, useSubmit} from "react-router-dom";
 
 const EventItem = ({event}) => {
+
+    // action 함수를 트리거하는 2번째 방법
+    const submit = useSubmit();
 
     const {
         'event-id': id,
@@ -22,22 +25,26 @@ const EventItem = ({event}) => {
     // };
 
     const deleteHandler = e => {
-
-        if (!window.confirm('정말 삭제하시겠습니까?')) return;
-
         console.log('id: ', id);
+
+        // action을 트리거
+        submit(null, {method: 'DELETE'});
+
+        /*
+        <Form method= 'delete'>
+         */
 
         // await 쓰려면 클릭 이벤트에 async 쓸 수 없어서 밖에서 함수 만들어서 불러오거나
         // 이렇게 즉시실행 함수로 써야함
-        (async () => {
-            await fetch(`http://localhost:8282/events/${id}`, {
-                method: 'DELETE'
-            });
-            navigate('/events');
-        })();
+        // (async () => {
+        //     await fetch(`http://localhost:8282/events/${id}`, {
+        //         method: 'DELETE'
+        //     });
+        //     navigate('/events');
+        // })();
+        // action이 fetch함
 
         // fetchDelete();
-
     };
 
     return (
--- src/components/RouteExample/pages/EventDetail.js
+++ src/components/RouteExample/pages/EventDetail.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {useLoaderData, useParams, useRouteLoaderData} from "react-router-dom";
+import {redirect, useLoaderData, useParams, useRouteLoaderData} from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 const EventDetail = () => {
@@ -9,7 +9,7 @@ const EventDetail = () => {
     // 사용범위가 본인 컴포넌트와 그 하위 컴포넌트 (children은 하위가 아님)
     const ev = useRouteLoaderData('event-detail'); // 부모의 loader를 불러오는 훅
 
-    return ( <EventItem event={ev} />  );
+    return (<EventItem event={ev}/>);
 }
 
 export default EventDetail;
@@ -26,11 +26,27 @@ export const loader = async ({params}) => {
 
     const response = await fetch(`http://localhost:8282/events/${id}`);
 
-    if(!response.ok) {
+    if (!response.ok) {
         //.. 예외 처리
     }
 
     return await response.json();
     // console.log('json: ', json);
 
+};
+
+// action을 트리거 하는 방법
+// 실제로 버튼이 있는 곳(EventItem.js)으로 이동
+export const action = async ({ params }) => {
+
+    if (!window.confirm('정말 삭제하시겠습니까?')) return;
+
+    const response = await fetch(`http://localhost:8282/events/${params.eventId}`, {
+        method: 'DELETE'
+    });
+
+    if(!response.ok) {
+        //...
+    }
+    return redirect('/events');
 };
