1943dbce21dd0feedd36adbbd678f752b138068d
SoobihnKim <ksbsj92@naver.com>
2024-07-19 11:09:59 +0900

    # 백엔드 연동 -	인터섹션옵저버를 통한 무한 스크롤

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './components/RouteExample/pages/Home';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import RootLayout from './components/RouteExample/layout/RootLayout';
 import ErrorPage from "./components/RouteExample/pages/ ErrorPage";
-import Events, { loader as eventListLoader } from "./components/RouteExample/pages/Events";
+import Events from "./components/RouteExample/pages/Events";
 import EventDetail, { loader as eventDetailLoader, action as deleteAction } from "./components/RouteExample/pages/EventDetail";
 import EventLayout from "./components/RouteExample/layout/EventLayout";
 import NewEvent from "./components/RouteExample/pages/NewEvent";
